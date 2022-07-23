c17dfcf723dfbfc6f49b71a5aae5998c2e3ec5ed
Dmitry Shalashov <skaurus@gmail.com>
2022-07-23 15:20:46 +0300

    docs: added example.go file, same as in README

--- cmd/example/example.go
+++ cmd/example/example.go
@@ -1,44 +1,3 @@
-# go-resque
-
-Simple [Resque](https://github.com/resque/resque) queue client for [Go](http://golang.org).
-
-## Introduction
-
-This is a fork of [go-resque](https://github.com/kavu/go-resque) (one of many).
-
-Differences from the original are:
-- it is a Go module
-- travis config is updated to use newer Go versions
-- all drivers except one removed
-- this driver is [go-redis](https://github.com/go-redis/redis) v9
-- driver update required to change some signatures to include context
-- also, in newer Redis' versions some commands are deprecated, and I used suggested replacements (namely, ZRANGE instead of ZRANGEBYSCORE)
-
-## Installation
-
-Installation is simple and familiar for Go programmers:
-
-```
-go get github.com/skaurus/go-resque
-```
-
-## Usage
-
-Let's assume that you have such Resque Job (taken from Resque examples):
-
-```ruby
-module Demo
-  class Job
-    def self.perform(params)
-      puts "Processed a job!"
-    end
-  end
-end
-```
-
-So, we can enqueue this job from Go.
-
-```go
 package main
 
 import (
@@ -79,10 +38,3 @@ func main() {
 		panic(err)
 	}
 }
-```
-
-Simple enough? I hope so.
-
-## Contributing
-
-Just open pull request or ping me directly on e-mail, if you want to discuss some ideas.
